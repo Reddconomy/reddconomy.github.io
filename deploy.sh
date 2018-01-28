232b4c02388a39487e2a6d1c7ccb7471e5d42fce
Riccardo Balbo <riccardo@forkforge.net>
2018-01-28 17:10:17 +0100

    Travis

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,32 @@
-source utils/git.sh
+#!/bin/bash
+echo "Generate..."
 ./generate.sh
+echo "Generated!"
+
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    exit 0
+fi
+
+echo "Deploy"
+
+rm -Rf .git
+rm -Rf tmp
+
+git clone https://${GH_TOKEN}@github.com/Reddconomy/reddconomy.github.io.git tmp
+cd tmp
+git checkout master
+cd ..
+
+mv tmp/.git dist/
+
+
 cd dist
-setup_git
-commit_website_files
-upload_files
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+git add .
+git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
+
+git remote add origin https://${GH_TOKEN}@github.com/Reddconomy/reddconomy.github.io.git > /dev/null 2>&1
+git push  origin master --force
